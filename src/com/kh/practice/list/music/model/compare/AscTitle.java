b0d15fb4a64c922540b0d1cde3c57c7519d31f8e
BaekUiHeon <qordmlgjs1@gmail.com>
2023-06-07 19:10:41 +0900

    정렬함수 주석처리
    이후 참고를 위한 주석처리

--- src/com/kh/practice/list/music/model/compare/AscTitle.java
+++ src/com/kh/practice/list/music/model/compare/AscTitle.java
@@ -2,7 +2,8 @@ package com.kh.practice.list.music.model.compare;
 import java.util.Comparator;
 import com.kh.practice.list.music.model.vo.Music;
 
-public class AscTitle implements Comparator<Music>{
+public class AscTitle implements Comparator<Music>{  // Collections.sort(list,정렬기준객체) 에서 정렬기준 객체를 만들기위한 클래스 Comparator인터페이스를 
+	                                                 //구현하며 compareTo함수를 이용해서 compare함수를 정의한다.
 	@Override
 	public int compare(Music music1, Music music2) {
         return music1.getTitle().compareTo(music2.getTitle());
